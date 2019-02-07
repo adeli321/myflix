a325c50145a4e18f62174277928ed64b74613384
Anthony Delivanis <anthony.delivanis@gmail.com>
2019-02-07 16:39:27 +0000

    add exact host to app run

--- video.py
+++ video.py
@@ -63,5 +63,5 @@ def play_video():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, host='localhost', port=8080)
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=8080)
 
