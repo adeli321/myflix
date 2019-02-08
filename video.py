e37e395ceb4fc96ca6671a3df74c6f7921259213
Anthony Delivanis <anthony.delivanis@gmail.com>
2019-02-08 23:38:18 +0000

    add thumbnails, edit py file, add html

--- video.py
+++ video.py
@@ -10,6 +10,8 @@ import json
 app = Flask(__name__)
 app.secret_key = b'_5-y4L"F4Q9z\n\x7ec]/'
 
+mongo_client = MongoClient('mongodb://restheart:R3ste4rt!@35.242.180.246:27017')
+
 @app.route('/', methods=['GET'])
 def entry() -> 'html':
     return render_template('homepage.html', title='MyFlix Welcomes You')
@@ -32,19 +34,66 @@ def moar_tests() -> 'html':
                                                 first=first,
                                                 last=last)
 
+@app.route('/testimage')
+def test_image() -> 'html':
+    return render_template('image_test.html')
+
+@app.route('/showvideo', methods=['POST', 'GET'])
+def show_video() -> 'video':
+    if request.method == 'POST':
+        session['video_title'] = request.form['title']
+        videos_db = mongo_client.get_database('videos')
+        fs        = GridFSBucket(videos_db)
+        grid_out  = fs.open_download_stream_by_name(session['video_title'])
+        contents  = grid_out.read()
+        return Response(contents, mimetype='video/mp4')
+    elif request.method == 'GET':
+        videos_db = mongo_client.get_database('videos')
+        fs        = GridFSBucket(videos_db)
+        grid_out  = fs.open_download_stream_by_name(session['video_title'])
+        contents  = grid_out.read()
+        return Response(contents, mimetype='video/mp4')
+
+
+# @route('/img/gridfs/<filename>')
+# def gridfs_img(filename):
+#     dbname = 'my_gridfs'
+#     db = connection[dbname]
+#     fs = gridfs.GridFS(db)
+#     thing = fs.get_last_version(filename=filename)
+#     response.content_type = 'image/jpeg'
+#     return thing
+
+
+@app.route('/img/gridfs/<filename>')
+def gridfs_img(filename):
+    images_db = mongo_client.get_database('thumbnails')
+    
+
+
+    fs        = GridFSBucket(images_db)
+    grid_out  = fs.open_download_stream_by_name(filename)
+    contents  = grid_out.read()
+    return Response(contents, mimetype='image/png')
+
+    # fs        = gridfs.GridFS(images_db)
+    # thing = fs.get_last_version(filename=filename)
+    # response.content_type = 'image/jpeg'
+    # return thing
+
+
+
 @app.route('/playvideo', methods=['POST', 'GET'])
 def play_video():
     if request.method == 'POST':
         session['video'] = request.form['movie_title'] 
-        client    = MongoClient('mongodb://restheart:R3ste4rt!@35.242.180.246:27017')
-        videos_db = client.get_database('videos')
+        videos_db = mongo_client.get_database('videos')
         fs        = GridFSBucket(videos_db)
         grid_out  = fs.open_download_stream_by_name(session['video'])
         contents  = grid_out.read()
         return Response(contents, mimetype='video/mp4')
     elif request.method == 'GET':
-        client    = MongoClient('mongodb://restheart:R3ste4rt!@35.242.180.246:27017')
-        videos_db = client.get_database('videos')
+        videos_db = mongo_client.get_database('videos')
         fs        = GridFSBucket(videos_db)
         grid_out  = fs.open_download_stream_by_name(session['video'])
         contents  = grid_out.read()
