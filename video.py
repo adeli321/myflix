8ab9e046c3b91f452f52612e85d0b2cd6ba28f4b
Anthony Delivanis <anthony.delivanis@gmail.com>
2019-02-09 00:21:08 +0000

    video.py changes

--- video.py
+++ video.py
@@ -5,12 +5,12 @@ from pymongo import MongoClient
 import gridfs
 from gridfs import GridFSBucket
 from flask import Flask, render_template, request, url_for, Response, redirect, session
-import json
+
 
 app = Flask(__name__)
 app.secret_key = b'_5-y4L"F4Q9z\n\x7ec]/'
 
-mongo_client = MongoClient('mongodb://restheart:R3ste4rt!@35.242.180.246:27017')
+mongo_client = MongoClient('mongodb://restheart:R3ste4rt!@35.246.41.186:27017')
 
 @app.route('/', methods=['GET'])
 def entry() -> 'html':
